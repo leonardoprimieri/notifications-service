5128482cda632a7913cec540d937dce48b248d01
Leonardo M. Primieri <leonardo.primieri@sof.to>
2022-12-18 11:03:02 -0300

    feat: create toDomain mapper

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
 import { Notification } from '@application/entities/notification';
+import { NotificationContent } from '@application/entities/notification-content';
+import { Notification as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -9,5 +11,19 @@ export class PrismaNotificationMapper {
       recipientId: notification.recipientId,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new NotificationContent(raw.content),
+        recipientId: raw.recipientId,
+        canceledAt: raw?.canceledAt,
+        createdAt: raw?.createdAt,
+        readAt: raw?.createdAt,
+      },
+      raw.id,
+    );
   }
 }
